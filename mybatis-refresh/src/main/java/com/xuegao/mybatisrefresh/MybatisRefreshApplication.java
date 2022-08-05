d0981dda54a2b06d6446823a18e8fc084f53f806
593436 <Xuegaoyu3>
2022-08-05 14:08:22 +0800

    mybatis

---
+++ mybatis-refresh/src/main/java/com/xuegao/mybatisrefresh/MybatisRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.mybatisrefresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisRefreshApplication.class, args);
+    }
+
+}
