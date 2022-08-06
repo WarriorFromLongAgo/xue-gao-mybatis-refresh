4ab78c8cba630c0864e2d7511cec5b632790dc27
593436 <Xuegaoyu3>
2022-08-06 18:26:10 +0800

    git

--- mybatis-demo/src/main/java/com/xuegao/mybatisdemo/business/demo/mapper/UserInfoMapper.java
+++ mybatis-demo/src/main/java/com/xuegao/mybatisdemo/business/demo/mapper/UserInfoMapper.java
@@ -1,4 +1,12 @@
 package com.xuegao.mybatisdemo.business.demo.mapper;
 
+import com.xuegao.mybatisdemo.business.demo.model.doo.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface UserInfoMapper {
+
+    List<UserInfo> list();
 }
