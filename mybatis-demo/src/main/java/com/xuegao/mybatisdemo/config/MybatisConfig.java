6fe8219fd637a28f6951131b11af14e692e68150
xuegao <hubeixuegao@gmail.com>
2022-08-07 14:55:08 +0800

    mybatis 整合完成

--- mybatis-demo/src/main/java/com/xuegao/mybatisdemo/config/MybatisConfig.java
+++ mybatis-demo/src/main/java/com/xuegao/mybatisdemo/config/MybatisConfig.java
@@ -1,7 +1,47 @@
-package com.xuegao.mybatisdemo.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MybatisConfig {
-}
+// package com.xuegao.mybatisdemo.config;
+//
+// import org.apache.ibatis.session.SqlSessionFactory;
+// import org.mybatis.spring.annotation.MapperScan;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Qualifier;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Primary;
+// import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+//
+// import javax.sql.DataSource;
+//
+// @Configuration
+// @MapperScan(basePackages = "com.xuegao.mybatisdemo.business.demo.*")
+// public class MybatisConfig {
+//     @Bean("sqlSessionFactory")
+//     @Primary
+//     public SqlSessionFactory sqlSessionFactory(@Autowired @Qualifier("dataSource") DataSource dataSource) throws Exception {
+//
+//         // 可添加自定义拦截器，没有自定义拦截器的小伙伴可忽略此部分
+//         // MybatisConfiguration
+//         MybatisConfiguration mybatisConfiguration = new MybatisConfiguration();
+//
+//         // 开启缓存，可自行选择
+//         mybatisConfiguration.setCacheEnabled(true);
+//         mybatisConfiguration.setMapUnderscoreToCamelCase(Boolean.TRUE);
+//
+//         // 使用MybatisSqlSessionFactoryBean
+//         MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
+//
+//         // 设置数据源
+//         sqlSessionFactoryBean.setDataSource(dataSource);
+//
+//         // 添加MybatisConfiguration
+//         sqlSessionFactoryBean.setConfiguration(mybatisConfiguration);
+//
+//         // 设置mapper相应的xml路径,
+//         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
+//                 .getResources("classpath*:/mapper/**/*.xml"));
+//         sqlSessionFactoryBean.setTypeAliasesPackage("com.kye.crmoffertool.model");
+//
+//         // 返回MybatisSqlSessionFactoryBean从而替代原生的sqlSessionFactory
+//         return sqlSessionFactoryBean.getObject();
+//     }
+//
+// }
--- mybatis-demo/src/test/java/com/xuegao/mybatisdemo/MybatisDemoApplicationTests.java
+++ mybatis-demo/src/test/java/com/xuegao/mybatisdemo/MybatisDemoApplicationTests.java
@@ -1,6 +1,6 @@
 package com.xuegao.mybatisdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
