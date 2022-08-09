a3bd704c05c9c330681bab52ac18248d2dea15cd
xuegao <hubeixuegao@gmail.com>
2022-08-09 23:18:11 +0800

    Revert "mybatis 整合完成"
    This reverts commit 96c7bb25

---
+++ mybatis-demo/src/test/java/com/xuegao/mybatisdemo/MybatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.mybatisdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
